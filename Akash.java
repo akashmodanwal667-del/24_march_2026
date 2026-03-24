8e125d1bddd4d5a8e5faea193c7eb893f039befc
Akash modanwal <akashmodanwal667@gmail.com>
2026-03-24 15:41:36 +0530

    Nayi file add ki

---
+++ Akash.java
@@ -0,0 +1,9 @@
+public class Akash {
+
+     public static void main(String args[]) {
+
+        System.out.print("I am Akash modanwal:");
+    }
+
+    
+}
---
+++ Iam.java
@@ -0,0 +1,8 @@
+public class Iam {
+
+      public static void main(String args[]) {
+
+        System.out.print("I am from Rameshwar varanasi:");
+    }
+    
+}
